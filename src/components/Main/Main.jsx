5079da78f0da213d34f38fdcc4fdd32bdc63336d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-20 16:52:17 +0200

    added button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import PostsList from "./PostsList";
+import Form from "../Form/Form";
+import { TiPlus } from "react-icons/ti";
+import { IoIosListBox } from "react-icons/io";
 
 const Main = () => {
+
+    const [isForm, setIsForm] = useState(false);
+
     return (
         <main>
-            <PostsList />
+
+            {/* Section Option */}
+            <section id="option">
+                <div className="d-flex justify-content-center gap-3">
+
+                    <button
+                        className="btn btn-primary d-flex align-items-center gap-1"
+                        onClick={() => setIsForm(curr => !curr)}
+                    >
+                        {isForm ?
+                            <>
+                                <IoIosListBox />Lista Posts
+                            </> :
+                            <>
+                                <TiPlus />Crea Post
+                            </>
+                        }
+                    </button>
+
+                </div>
+            </section>
+
+            {
+                isForm ?
+                    <Form /> :
+                    <PostsList />
+            }
         </main>
     )
 }
